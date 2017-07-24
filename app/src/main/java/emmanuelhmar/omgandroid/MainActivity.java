479321e57e36a56739c180793b297a236e40e09b
Emmanuel Hmar <eman.hmar@gmail.com>
2017-07-24 16:31:08 -0400

    Create an Action Bar

--- app/src/main/java/emmanuelhmar/omgandroid/MainActivity.java
+++ app/src/main/java/emmanuelhmar/omgandroid/MainActivity.java
@@ -1,9 +1,13 @@
 package emmanuelhmar.omgandroid;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,8 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button mainButton;
     EditText mainEditText;
     ListView mainListView;
-    ArrayAdapter<String> mArrayAdapter;
-    ArrayList<String> mNameList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> nameList = new ArrayList<>();
+    ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +44,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mainListView = (ListView) findViewById(R.id.main_listView);
 
 //        Create an ArrayAdapter for the ListView
-        mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNameList);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameList);
 
 //        Set the Listview to use the ArrayAdapter
-        mainListView.setAdapter(mArrayAdapter);
+        mainListView.setAdapter(arrayAdapter);
 
         //5. Set this activity to react to list items being pressed
         mainListView.setOnItemClickListener(this);
@@ -51,10 +56,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //inflate the menu; this adds items to the action bar if it present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+        //inflate the menu_main; this adds items to the action bar if it present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+//        Access the object responsible for putting together the sharing submenu
+        if (shareItem != null) {
+            shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+//        Create an Intent to share your content
+        setShareIntent();
+
         return true;
     }
+
+    private void setShareIntent(){
+        if (shareActionProvider != null) {
+//            Create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+//            Make sure the provider knows it should work with that intent
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -64,14 +94,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mainTextView.setText(text);
 
 //        Also add that value to the list shown in ListView
-        mNameList.add(mainEditText.getText().toString());
-        mArrayAdapter.notifyDataSetChanged();
+        nameList.add(mainEditText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+//        6. The text you'd like to share has changed and you need to update
+        setShareIntent();
     }
 
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 //        Log the item's position and contents to the console in Debug
-        Log.d("omg Android", position + ":" + mNameList.get(position));
+        Log.d("omg Android", position + ":" + nameList.get(position));
     }
 }
